a9bb6afb7940db411fa36500cb4d07d70bc6057a
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-03-27 20:18:17 +0530

    add routings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// Page Components
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import QuizPage from "./pages/quizPage";
 
-  return <>Initial Code!</>;
+import { Routes, Route } from "react-router";
+
+function App() {
+  const isLoggedIn = false; // Check if user exists
+
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={!isLoggedIn ? <LoginPage /> : <HomePage />}
+      />
+      <Route path="/" element={isLoggedIn ? <HomePage /> : <LoginPage />} />
+      <Route
+        path="/quiz/:quizId"
+        element={isLoggedIn ? <QuizPage /> : <LoginPage />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
